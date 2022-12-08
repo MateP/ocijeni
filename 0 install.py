b890b672b13fed0f18f04b13b63397b188996e17
MateP <puljizmate@gmail.com>
2022-12-08 13:46:12 +0100

    Added pillow as a requirement.

--- 0 install.py
+++ 0 install.py
@@ -8,7 +8,7 @@ from tkinter import messagebox
 try:
     subprocess.check_call(
         [sys.executable, '-m', 'pip', 'install', '-U', 'pip'])
-    for package in ['openpyxl']:
+    for package in ['pillow', 'openpyxl']:
         subprocess.check_call(
             [sys.executable, '-m', 'pip', 'install', '-U', package])
 
